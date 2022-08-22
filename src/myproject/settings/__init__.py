e9a6dd66b87f1c65d7682b5ef07afde62fb32f95
MahmudulHassan5809 <mahmudul.hassan240@gmail.com>
2022-08-22 18:21:58 +0600

    fix: fix import issue

--- src/myproject/settings/__init__.py
+++ src/myproject/settings/__init__.py
@@ -1,5 +1,7 @@
 from myproject.settings.base import *  # NOQA
 from myproject.settings.app_vars import *  # NOQA
 from myproject.settings.apps import *  # NOQA
-from myproject.settings.database import *  # NOQA
+
+# We dont need database in this project
+# from myproject.settings.database import *  # NOQA
 from myproject.settings.logger import *  # NOQA
